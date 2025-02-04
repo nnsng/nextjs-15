cac3c1472da7233a6dd3f139e1c783670b12a210
nnsng <ngocsang1201@gmail.com>
2025-02-04 22:36:07 +0700

    not found page

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,9 +1,15 @@
+import { notFound } from 'next/navigation';
+
 export default async function ProductReview({
   params,
 }: {
   params: Promise<{ productId: string; reviewId: string }>;
 }) {
   const { productId, reviewId } = await params;
+  if (parseInt(reviewId) > 1000) {
+    notFound();
+  }
+
   return (
     <div>
       Review {reviewId} for product {productId}
