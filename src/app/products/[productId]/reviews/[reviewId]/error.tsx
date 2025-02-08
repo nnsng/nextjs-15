42349a9467b8aa3ae3c084cfaa6f0c6ad9eef17f
nnsng <ngocsang1201@gmail.com>
2025-02-08 22:15:28 +0700

    error handling

---
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function ErrorBoundary({ error }: { error: Error }) {
+  return (
+    <div>
+      <h1>Something went wrong</h1>
+      <p>{error.message}</p>
+    </div>
+  );
+}
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,14 +1,20 @@
-import { notFound, redirect } from 'next/navigation';
+import { notFound } from 'next/navigation';
+
+const getRandomInt = (max: number) => Math.floor(Math.random() * max);
 
 export default async function ProductReview({
   params,
 }: {
   params: Promise<{ productId: string; reviewId: string }>;
 }) {
+  const random = getRandomInt(2);
+  if (random === 1) {
+    throw new Error('Error in ProductReview');
+  }
+
   const { productId, reviewId } = await params;
   if (parseInt(reviewId) > 1000) {
-    // notFound();
-    redirect('/products');
+    notFound();
   }
 
   return (
