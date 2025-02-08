d75987a5873ea7451bbba818e740bd31f445cf5b
nnsng <ngocsang1201@gmail.com>
2025-02-08 22:24:01 +0700

    recovering from errors

--- src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,10 +1,23 @@
 'use client';
 
-export default function ErrorBoundary({ error }: { error: Error }) {
+import { useRouter } from 'next/navigation';
+import { startTransition } from 'react';
+
+export default function ErrorBoundary({ error, reset }: { error: Error; reset: () => void }) {
+  const router = useRouter();
+
+  const reload = () => {
+    startTransition(() => {
+      router.refresh();
+      reset();
+    });
+  };
+
   return (
     <div>
       <h1>Something went wrong</h1>
       <p>{error.message}</p>
+      <button onClick={reload}>Try again</button>
     </div>
   );
 }
