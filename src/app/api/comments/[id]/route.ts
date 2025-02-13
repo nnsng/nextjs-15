abbc21353197363a05baa7f36395ec9bc980e6b6
nnsng <ngocsang1201@gmail.com>
2025-02-13 22:37:01 +0700

    handling DELETE requests

--- src/app/api/comments/[id]/route.ts
+++ src/app/api/comments/[id]/route.ts
@@ -14,3 +14,11 @@ export async function PATCH(request: Request, { params }: { params: Promise<{ id
   comments[index].text = text;
   return Response.json(comments[index]);
 }
+
+export async function DELETE(_request: Request, { params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  const index = comments.findIndex((comment) => comment.id === parseInt(id));
+  const deletedComment = comments[index];
+  comments.splice(index, 1);
+  return Response.json(deletedComment);
+}
