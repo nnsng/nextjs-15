000a5eb0d685155eff60b00172c7b7628399652b
nnsng <ngocsang1201@gmail.com>
2025-03-11 21:59:57 +0700

    dynamic rendering

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,10 @@
-export default function About() {
+import { cookies } from 'next/headers';
+
+export default async function About() {
+  const cookieStore = await cookies();
+  const theme = cookieStore.get('theme');
+  console.log(theme);
+
   return (
     <div>
       <h1>About</h1>
