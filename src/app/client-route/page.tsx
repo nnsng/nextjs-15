e7e89c15621ae972f2bf74ed95e7585e02c43dae
nnsng <ngocsang1201@gmail.com>
2025-03-25 21:38:53 +0700

    client-only code

--- src/app/client-route/page.tsx
+++ src/app/client-route/page.tsx
@@ -2,12 +2,14 @@
 
 import ImageSlider from '@/components/ImageSlider';
 import { useTheme } from '@/components/ThemeProvider';
+import { clientSideFunc } from '@/utils/client-utils';
 
 export default function ClientRoute() {
   const theme = useTheme();
+  const result = clientSideFunc();
   return (
     <>
-      <h1 style={{ color: theme.colors.primary }}>Client Route</h1>
+      <h1 style={{ color: theme.colors.primary }}>Client Route {result}</h1>
       <ImageSlider />
     </>
   );
--- src/app/server-route/page.tsx
+++ src/app/server-route/page.tsx
@@ -3,6 +3,7 @@ import { serverSideFunc } from '@/utils/server-utils';
 
 export default function ServerRoute() {
   const result = serverSideFunc();
+
   return (
     <>
       <h1>Server Route {result}</h1>
