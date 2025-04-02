0b917e2326edb099e21861d1c010c0a29494b42d
nnsng <ngocsang1201@gmail.com>
2025-04-02 22:42:05 +0700

    cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import { Search } from '@/components/search';
-
 export default function Home() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <Search />
-    </div>
-  );
+  return <div></div>;
 }
