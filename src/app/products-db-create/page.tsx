a34c0315eccfa02b51c7609f5d2611eca3e1abaa
nnsng <ngocsang1201@gmail.com>
2025-04-02 16:19:43 +0700

    separating server actions

--- src/app/products-db-create/page.tsx
+++ src/app/products-db-create/page.tsx
@@ -1,19 +1,7 @@
 'use client';
 
-// import { Submit } from '@/components/submit';
-import { addProduct } from '@/prisma-db';
-import { redirect } from 'next/navigation';
+import { createProduct, type FormState } from '@/actions/products';
 import { useActionState } from 'react';
-
-type Errors = {
-  title?: string;
-  price?: string;
-  description?: string;
-};
-
-type FormState = {
-  errors: Errors;
-};
 
 export default function CreateProductPage() {
   const initialState: FormState = {
@@ -21,26 +9,6 @@ export default function CreateProductPage() {
   };
 
   const [state, formAction, isPending] = useActionState(createProduct, initialState);
-
-  const createProduct = async (formData: FormData) => {
-    'use server';
-
-    const title = formData.get('title') as string;
-    const price = formData.get('price') as string;
-    const description = formData.get('description') as string;
-
-    const errors: Errors = {};
-    if (!title) errors.title = 'Title is required';
-    if (!price) errors.price = 'Price is required';
-    if (!description) errors.description = 'Description is required';
-
-    if (Object.keys(errors).length > 0) {
-      return { errors };
-    }
-
-    await addProduct(title, parseInt(price), description);
-    redirect('/products-db');
-  };
 
   return (
     <form action={formAction} className="max-w-96 space-y-4 p-4">
